b9eb268a9db3ec2ed07b1752bdd1c882ed8bc6c3
Alex Leybourne <alexleybourne1@gmail.com>
2019-09-20 15:40:51 +1000

    Menu animation updiate and accessability additions

--- src/scripts/accessability.js
+++ src/scripts/accessability.js
@@ -1,9 +1,19 @@
-  document.querySelector("#toggler").addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
+document.querySelector("#toggler").addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
     event.preventDefault();
     document.getElementById("toggler").click();
-    }
-  });
+  }
+});
 
-  document.querySelector("#toggler").onfocus = () => document.querySelector("#hamburger").style.border = "1px solid red"
-  document.querySelector("#toggler").onblur = () => document.querySelector("#hamburger").style.border = "none"
+document.querySelector("#toggler").addEventListener("click", () => {
+  document.querySelector(".menu").classList.toggle("open");
+  document.querySelector("#hamburger").style.border = "none";
+});
+
+document.querySelector("#toggler").onfocus = () => {
+  document.querySelector("#hamburger").style.border = "1px solid black";
+};
+
+document.querySelector("#toggler").onblur = () => {
+  document.querySelector("#hamburger").style.border = "none";
+};
