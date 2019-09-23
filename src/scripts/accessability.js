7c2ffd2a5c60dbc7ada3254986b8f899f46361bf
Alex Leybourne <alexleybourne1@gmail.com>
2019-09-23 16:45:42 +1000

    Removed Animated Background

---
+++ src/scripts/accessability.js
@@ -0,0 +1,18 @@
+document.querySelector("#toggler").addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("toggler").click();
+  }
+});
+
+document.querySelector("#toggler").addEventListener("click", () => {
+  document.querySelector("#hamburger").style.border = "none";
+});
+
+document.querySelector("#toggler").onfocus = () => {
+  document.querySelector("#hamburger").style.border = "1px solid black";
+};
+
+document.querySelector("#toggler").onblur = () => {
+  document.querySelector("#hamburger").style.border = "none";
+};
